95ff43de6e352056f4f228df2e411d8419e9cd0c
KariDriff <106455500+KariDriff@users.noreply.github.com>
2022-07-13 02:33:21 -0300

    Class Livro

--- src/br/com/dio/First.java
+++ src/br/com/dio/First.java
@@ -7,11 +7,46 @@ public class First {
         Gato gato = new Gato();
         System.out.println(gato);
 
-
+        Livro livro1 = new Livro ("Livro1", 200);
+        System.out.println(livro1);
 
 /*      int a = 5;
         int b = 3;
         System.out.println("Hello, World!" + (a+b));
 */
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
